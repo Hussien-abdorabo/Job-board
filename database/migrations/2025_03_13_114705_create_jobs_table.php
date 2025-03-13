32cbb95884dbfe367f7533f34e22b9b6b5a7a12f
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-03-13 14:43:38 +0200

    Create 2025_03_13_114705_create_jobs_table.php
    create new job migration

---
+++ database/migrations/2025_03_13_114705_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Job;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->enum('category',Job::$categories);
+            $table->unsignedInteger('salary');
+            $table->enum('experience', Job::$experiences);
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
