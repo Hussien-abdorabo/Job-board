a6aa68499de32db817fca55442bc85d6d0816866
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-03-14 13:57:05 +0200

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register Passport routes
+//        Passport::routes();
     }
 }
