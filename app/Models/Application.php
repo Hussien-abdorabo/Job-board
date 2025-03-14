123d617775d99570c5195f0c572598d3a8a9162a
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-03-14 14:49:14 +0200

    Create Application.php
    add application relationships

---
+++ app/Models/Application.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    /** @use HasFactory<\Database\Factories\ApplicationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'job_id',
+        'resume_path',
+        'cover_letter',
+        'status',
+    ];
+
+    protected $casts = [
+        'status'=> 'string'
+    ];
+
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+    public function job(){
+        return $this->belongsTo(Job::class,'job_id');
+    }
+}
