9d3b2939dceb221e2d013a69bcd34dac919fa8e5
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-03-15 14:57:28 +0200

    create new job and Retrieve a list of all job postings (publicly accessible)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
@@ -8,5 +9,14 @@ Route::prefix('auth')->group(function () {
     // API routes will go here
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 });
+Route::prefix('jobs')->group(function () {
+        Route::get('list',[JobController::class, 'index']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::Post('create', [JobController::class, 'store']);
+    });
+});
+
