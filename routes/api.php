eae15ca58995488174f54f2a711ad249c86986d2
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-03-17 09:55:45 +0200

    Update api.php
    add application apply route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\API\ApplicationController;
 
 
 Route::prefix('auth')->group(function () {
@@ -17,6 +18,7 @@ Route::prefix('jobs')->group(function () {
         Route::get('list',[JobController::class, 'index']);
     Route::middleware('auth:sanctum')->group(function () {
         Route::Post('create', [JobController::class, 'store']);
+        Route::post('{job}/apply', [ApplicationController::class, 'store']);
     });
 });
 
