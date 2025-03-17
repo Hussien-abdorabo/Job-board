ba799f3c3a7093ef3bc51a00d989e62ea7321cdd
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-03-17 12:52:30 +0200

    add function that enable user to see his application statis

--- app/Http/Controllers/Api/ApplicationController.php
+++ app/Http/Controllers/Api/ApplicationController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Events\ApplicationStatusUpdate;
 use App\Http\Controllers\Controller;
 use App\Models\Application;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Job;
+use Illuminate\Validation\Rule;
 
 class ApplicationController extends Controller
 {
@@ -36,7 +39,7 @@ class ApplicationController extends Controller
         if($validator->fails()){
             return response()->json([
                 'errors' => $validator->errors()->all(),
-            ]);
+            ],422);
         }
         $resume_path =$request->file('resume_path')->store('resume','public');
         $application = Application::create([
@@ -47,23 +50,68 @@ class ApplicationController extends Controller
         ]);
         return response()->json([
             'message' => 'Application Submitted successfully',
-        ]);
+        ],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Application $application)
     {
-        //
+        $user = auth()->user();
+
+        if(!$user || $user->role !== 'job_seeker'){
+            return response()->json(['error' => 'only job_seeker can show their application status'], 401);
+        }
+        if ($user->id !== $application->user_id){
+            return response()->json(['error' => 'you are not the owner of this applicaton'], 401);
+        }
+
+        $application = Application::where('id',$application->id)->first();
+        return response()->json([
+            'job' => $application->job,
+            'application_status' => $application->status,
+        ],201);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Application $application)
     {
-        //
+        $user = auth()->user();
+        $job = $application->job;
+        if (!$user || $user->role !=='employer') {
+            return response()->json(['error' => 'only employer can update that'], 401);
+        }
+        if($user->id !== $job->user_id){
+            return response()->json(['error' => 'you are not the employer who post this job of this job'], 401);
+        }
+        $validator = Validator::make($request->all(),rules: array(
+            'status' => "required|",[Rule::in(Application::$statuses)]
+        ));
+        if($validator->fails()){
+            return response()->json([
+                'errors' => $validator->errors()->all(),
+            ]);
+        }
+        $old_status = $application->status;
+        $application->update([
+            'status' => $request->status,
+        ]);
+        Log::info("Application Status Updated",[
+            'job_id' => $job->id,
+            'user_id' => $job->user_id,
+            'application_id'=> $application->id,
+            'old_status' => $old_status,
+            'status' => $request->status,
+        ]);
+        event(new ApplicationStatusUpdate($application));
+
+        return response()->json([
+            'message' => 'Application Status Updated successfully',
+            'status' => $application->status,
+        ]);
     }
 
     /**
@@ -71,6 +119,6 @@ class ApplicationController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+
     }
 }
